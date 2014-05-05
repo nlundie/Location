16891d80c698b2c31e9bef749a61b0912fd87554
Lundie <ngl142@gmail.com>
2014-05-05 13:16:36 -0400

    Initial Commit

---
+++ Location/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Location
+//
+//  Created by Nate Lundie on 5/5/14.
+//  Copyright (c) 2014 Lundie Sutton Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
