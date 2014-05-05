991308798e3229c8556ba71f8b355780fc64f28f
Nate Lundie <ngl142@gmail.com>
2014-05-05 17:05:35 -0400

    basic app that prints out your location data
    shows lat, long, and elevation among other things.

--- Location/ViewController.h
+++ Location/ViewController.h
@@ -7,7 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+@property (strong, nonatomic) IBOutlet UILabel *latitude;
+@property (strong, nonatomic) IBOutlet UILabel *longitude;
+@property (strong, nonatomic) IBOutlet UILabel *horizontalAccuracy;
+@property (strong, nonatomic) IBOutlet UILabel *verticalAccuracy;
+@property (strong, nonatomic) IBOutlet UILabel *altitude;
+@property (strong, nonatomic) IBOutlet UIButton *butt;
+@property (strong, nonatomic) IBOutlet UILabel *distance;
+@property (strong, nonatomic) NSMutableArray *latArray;
+@property (strong, nonatomic) NSMutableArray *longArray;
+@property (strong, nonatomic) NSMutableArray *altArray;
+
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLLocation *startLocation;
+- (IBAction)resetDistance:(id)sender;
+
+
 
 @end
--- Location/ViewController.m
+++ Location/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "CoreLocation/CoreLocation.h"
 
 @interface ViewController ()
 
@@ -14,9 +15,35 @@
 
 @implementation ViewController
 
+@synthesize locationManager;
+@synthesize latitude;
+@synthesize longitude;
+@synthesize horizontalAccuracy;
+@synthesize verticalAccuracy;
+@synthesize altitude;
+@synthesize startLocation;
+@synthesize distance;
+@synthesize latArray;
+@synthesize longArray;
+@synthesize altArray;
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
+    startLocation = nil;
+    altArray = [[NSMutableArray alloc] init];
+    latArray = [[NSMutableArray alloc] init];
+    longArray = [[NSMutableArray alloc] init];
+    NSInteger *count;
+    count = 0;
+    
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +51,67 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+#pragma mark CLLocationManagerDelegate
+
+-(void)locationManager:(CLLocationManager *)manager
+   didUpdateToLocation:(CLLocation *)newLocation
+          fromLocation:(CLLocation *)oldLocation
+{
+    NSString *currentLatitude = [[NSString alloc]
+                                 initWithFormat:@"%+.6f",
+                                 newLocation.coordinate.latitude];
+    latitude.text = currentLatitude;
+    
+    NSString *currentLongitude = [[NSString alloc]
+                                  initWithFormat:@"%+.6f",
+                                  newLocation.coordinate.longitude];
+    longitude.text = currentLongitude;
+    
+    NSString *currentHorizontalAccuracy =
+    [[NSString alloc]
+     initWithFormat:@"%+.6f",
+     newLocation.horizontalAccuracy];
+    horizontalAccuracy.text = currentHorizontalAccuracy;
+    
+    NSString *currentAltitude = [[NSString alloc]
+                                 initWithFormat:@"%+.6f",
+                                 newLocation.altitude];
+    altitude.text = currentAltitude;
+    //altArray = [[NSMutableArray alloc] init];
+    //[altArray addObject:currentAltitude];
+    //NSLog(@"altArray: %@", altArray);
+    
+    NSString *currentVerticalAccuracy =
+    [[NSString alloc]
+     initWithFormat:@"%+.6f",
+     newLocation.verticalAccuracy];
+    verticalAccuracy.text = currentVerticalAccuracy;
+    
+    if (startLocation == nil)
+        startLocation = newLocation;
+    
+    CLLocationDistance distanceBetween = [newLocation
+                                          distanceFromLocation:startLocation];
+    
+    NSString *tripString = [[NSString alloc]
+                           initWithFormat:@"%f",
+                            distanceBetween];
+    distance.text = tripString;
+    [latArray addObject:currentLatitude];
+    NSLog(@"latArray: %@", latArray);
+    [longArray addObject:currentLongitude];
+    NSLog(@"longArray: %@", longArray);
+}
+- (IBAction)buttonstuff:(id)sender {
+    //altArray = [[NSMutableArray alloc] init];
+    [altArray addObject:altitude.text];
+    
+    NSLog(@"altArray: %@", altArray);
+    
+    
 }
 
 @end
